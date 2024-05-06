83975c36e318abdd38482806a02f4978a3ae4516
msantos-lab <msantosdev.lab@gmail.com>
2024-05-06 16:56:51 +0200

    first commit

---
+++ src/main/java/com/devsuperior/desafiomdo/DesafiomdoApplication.java
@@ -0,0 +1,13 @@
+package com.devsuperior.desafiomdo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DesafiomdoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DesafiomdoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/devsuperior/desafiomdo/DesafiomdoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devsuperior.desafiomdo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DesafiomdoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
